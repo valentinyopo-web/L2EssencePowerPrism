b8b1a98fc70700b2e98b42a79e91b2ecbc7dc9f3
valentinyopo-web <valentin.yopo@gmail.com>
2026-07-26 22:59:18 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     // Proxy API calls to Express during local development
     proxy: {
       '/api': {
-        target: 'http://localhost:10000',
+        target: 'http://localhost:10000/api/',
         changeOrigin: true,
       },
     },
